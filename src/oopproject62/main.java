155aedd0b21c2c97ba5143d0c5294cd896e9b461
SarunyuRuenruay <sarunyu.n17@gmail.com>
2019-11-15 18:02:03 +0700

    edit

--- src/oopproject62/main.java
+++ src/oopproject62/main.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oopproject62;
 
 /**
  *
- * @author Jame
+ * @author prize
  */
-public class main {
-    public static void main(String[] arg){
-        System.out.println("jaem");
+public class Main {
+    
+    public static void main(String[] args) {
+        new GameUI().init();
     }
 }
