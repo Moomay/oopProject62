f8ccdd97bbceea6ae98d85f4049fcaba47e3f1da
SarunyuRuenruay <sarunyu.n17@gmail.com>
2019-11-16 14:42:27 +0700

    draw

--- src/oopproject62/GameController.java
+++ src/oopproject62/GameController.java
@@ -5,19 +5,90 @@
  */
 package oopproject62;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author Jame
  */
-public class GameController implements Runnable{
+public class GameController implements Runnable, ActionListener {
+
+    private BufferedImage testImage;
+    private GameView view;
     
-    public static void main(String[] args) {
-        GameView view = new GameView(1920, 800);
+    private Thread thread0;
+    private boolean running = false;
+    
+    private BufferStrategy bs;
+    private Graphics g;
+    
+    public GameController() {
+
+    }
+
+    private void init() {
+        view = new GameView(700, 500);
         view.init();
     }
 
-    @Override
+    private void tick() {
+    }
+
+    private void render() {
+        bs = view.getC1().getBufferStrategy();
+        if (bs == null){
+            view.getC1().createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+        //Claer Screen
+        g.clearRect(0,0, 700, 500);
+        //Draw
+        
+        g.drawRect(20, 10, 50, 200);
+        
+        //end Drawing
+        bs.show();
+        g.dispose();
+    }
+
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        init();
+        while (running) {
+            tick();
+            render();
+        }
+        stop();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread0 = new Thread(this);
+        thread0.start();
+    }
+
+    public synchronized void stop() {
+        if (!running) {
+            return;
+        }
+        running = false;
+        try {
+            thread0.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GameController game = new GameController();
+        game.start();
     }
 }
--- src/oopproject62/GameView.java
+++ src/oopproject62/GameView.java
@@ -1,3 +1,4 @@
+package oopproject62;
 
 import java.awt.*;
 import javax.swing.*;
@@ -7,26 +8,29 @@ import javax.swing.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author prize
  */
 public class GameView extends JPanel {
-    
-    private int width, height; 
+
+    private int width, height;
     private JFrame f1;
+    private Canvas c1;
+
     private JMenuBar mb1;
     private JMenu m1, m2;
-    private JMenuItem mi1,mi2,mi3,mi4,mi5;
-    public GameView() {}
-    
-    public GameView(int width,int height){
+    private JMenuItem mi1, mi2, mi3, mi4, mi5;
+
+    public GameView() {
+    }
+
+    public GameView(int width, int height) {
         this.width = width;
         this.height = height;
     }
-    
-    public void init(){
+
+    public void init() {
         // step 1
         f1 = new JFrame("Word Of The Death");
         
@@ -41,26 +45,128 @@ public class GameView extends JPanel {
         mi4 = new JMenuItem("Window");
         mi5 = new JMenuItem("Message");
         
+        c1 = new Canvas();
         // step 2
         f1.setSize(width, height);
         f1.setVisible(true);
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f1.setBackground(Color.black);
         
+        c1.setPreferredSize(new Dimension(width, height));
+        c1.setMaximumSize(new Dimension(width, height));
+        c1.setMinimumSize(new Dimension(width, height));
+
         // step 3
         f1.setJMenuBar(mb1);
-        
+
         mb1.add(m1);
-        
+
         m1.add(m2);
         m1.add(mi1);
         m1.add(mi2);
         m1.add(mi3);
-        
+
         m2.add(mi4);
         m2.add(mi5);
-        
+        f1.add(c1);
         f1.getContentPane().setBackground(Color.black);
+        f1.pack();
     }
-    
+
+    public Canvas getC1() {
+        return c1;
+    }
+
+    public void setC1(Canvas c1) {
+        this.c1 = c1;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public JFrame getF1() {
+        return f1;
+    }
+
+    public void setF1(JFrame f1) {
+        this.f1 = f1;
+    }
+
+    public JMenuBar getMb1() {
+        return mb1;
+    }
+
+    public void setMb1(JMenuBar mb1) {
+        this.mb1 = mb1;
+    }
+
+    public JMenu getM1() {
+        return m1;
+    }
+
+    public void setM1(JMenu m1) {
+        this.m1 = m1;
+    }
+
+    public JMenu getM2() {
+        return m2;
+    }
+
+    public void setM2(JMenu m2) {
+        this.m2 = m2;
+    }
+
+    public JMenuItem getMi1() {
+        return mi1;
+    }
+
+    public void setMi1(JMenuItem mi1) {
+        this.mi1 = mi1;
+    }
+
+    public JMenuItem getMi2() {
+        return mi2;
+    }
+
+    public void setMi2(JMenuItem mi2) {
+        this.mi2 = mi2;
+    }
+
+    public JMenuItem getMi3() {
+        return mi3;
+    }
+
+    public void setMi3(JMenuItem mi3) {
+        this.mi3 = mi3;
+    }
+
+    public JMenuItem getMi4() {
+        return mi4;
+    }
+
+    public void setMi4(JMenuItem mi4) {
+        this.mi4 = mi4;
+    }
+
+    public JMenuItem getMi5() {
+        return mi5;
+    }
+
+    public void setMi5(JMenuItem mi5) {
+        this.mi5 = mi5;
+    }
+
 }
