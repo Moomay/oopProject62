67745edf06fb3a42311b05bede170dd67a6fc4c0
Sroravit Tamsamak <sroravitoak@hotmail.com>
2019-11-21 23:35:41 +0700

    Startgame

--- src/GameOOP/Startgame.java
+++ src/GameOOP/Startgame.java
@@ -27,8 +27,12 @@ public class Startgame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jInternalFrame1 = new javax.swing.JInternalFrame();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Word of the Death");
@@ -36,7 +40,21 @@ public class Startgame extends javax.swing.JFrame {
         setPreferredSize(new java.awt.Dimension(1280, 720));
         getContentPane().setLayout(null);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/Gamebg2.jpg"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/Gamebg2.jpg"))); // NOI18N
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel1MouseReleased(evt);
+            }
+        });
+        jLabel1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jLabel1KeyReleased(evt);
+            }
+        });
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 1280, 720);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/story1.jpg"))); // NOI18N
         jLabel2.setText("jLabel2");
         jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseReleased(java.awt.event.MouseEvent evt) {
@@ -44,22 +62,80 @@ public class Startgame extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(0, 0, 1318, 720);
+        jLabel2.setBounds(0, 0, 1280, 720);
 
-        jInternalFrame1.setVisible(true);
-        getContentPane().add(jInternalFrame1);
-        jInternalFrame1.setBounds(210, 110, 22, 34);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/story2.jpg"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel3MouseReleased(evt);
+            }
+        });
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(0, 0, 1280, 720);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/story3.jpg"))); // NOI18N
+        jLabel4.setText("jLabel4");
+        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel4MouseReleased(evt);
+            }
+        });
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 0, 1280, 720);
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/story4.jpg"))); // NOI18N
+        jLabel5.setText("jLabel5");
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel5MouseReleased(evt);
+            }
+        });
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(0, 0, 1280, 720);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GameOOP/story5.jpg"))); // NOI18N
+        jLabel6.setText("jLabel6");
+        jLabel6.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel6MouseReleased(evt);
+            }
+        });
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(0, 0, 1280, 720);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jLabel2MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseReleased
+    private void jLabel1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jLabel1KeyReleased
+        jLabel1.setVisible(false);        
+    }//GEN-LAST:event_jLabel1KeyReleased
 
+    private void jLabel1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseReleased
+        jLabel1.setVisible(false);    
+    }//GEN-LAST:event_jLabel1MouseReleased
+
+    private void jLabel2MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseReleased
+        jLabel2.setVisible(false);      
+    }//GEN-LAST:event_jLabel2MouseReleased
+
+    private void jLabel3MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseReleased
+        jLabel3.setVisible(false);
+    }//GEN-LAST:event_jLabel3MouseReleased
+
+    private void jLabel5MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseReleased
+        jLabel5.setVisible(false);
+    }//GEN-LAST:event_jLabel5MouseReleased
+
+    private void jLabel4MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseReleased
+        jLabel4.setVisible(false);
+    }//GEN-LAST:event_jLabel4MouseReleased
+
+    private void jLabel6MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel6MouseReleased
+        this.setVisible(false);
         GameController game = new GameController();
         game.start();
-        this.setVisible(false);
-        
-    }//GEN-LAST:event_jLabel2MouseReleased
+    }//GEN-LAST:event_jLabel6MouseReleased
 
     /**
      * @param args the command line arguments
@@ -97,7 +173,11 @@ public class Startgame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JInternalFrame jInternalFrame1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     // End of variables declaration//GEN-END:variables
 }
